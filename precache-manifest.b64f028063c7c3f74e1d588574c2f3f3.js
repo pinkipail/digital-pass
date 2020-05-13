d57a4c84ba2506faa40ce9051cea03f54d700311
pinkipail <fyodorov_vasya@list.ru>
2020-05-13 17:36:09 +0300

    Updates

---
+++ precache-manifest.b64f028063c7c3f74e1d588574c2f3f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381400efb8c5cfd3889be8b3eab43c19",
+    "url": "/digital-pass/index.html"
+  },
+  {
+    "revision": "42ea57ed84f11cd1ca99",
+    "url": "/digital-pass/static/css/2.ce41e2b0.chunk.css"
+  },
+  {
+    "revision": "4b14f892fc8a4e4b5fd5",
+    "url": "/digital-pass/static/css/main.09fe75f9.chunk.css"
+  },
+  {
+    "revision": "42ea57ed84f11cd1ca99",
+    "url": "/digital-pass/static/js/2.d74eafd3.chunk.js"
+  },
+  {
+    "revision": "903aff72049b10ecfe82e3d967bc22d3",
+    "url": "/digital-pass/static/js/2.d74eafd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b14f892fc8a4e4b5fd5",
+    "url": "/digital-pass/static/js/main.4f491653.chunk.js"
+  },
+  {
+    "revision": "604f02437a56c23d8c56",
+    "url": "/digital-pass/static/js/runtime-main.549e952c.js"
+  }
+]);
